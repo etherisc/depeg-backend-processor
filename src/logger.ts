45c99388d62603a87c856fdb8a1da870dab3ee68
Marc Doerflinger <marc@doerf.li>
2023-07-07 07:07:25 +0000

    show timestamp in log

--- src/logger.ts
+++ src/logger.ts
@@ -2,13 +2,14 @@ import winston from "winston";
 
 const logger = winston.createLogger({
     level: 'info',
-    format: winston.format.json(),
-    // defaultMeta: { service: 'user-service' },
+    format: winston.format.combine(
+        winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss"}),
+        winston.format.json(),
+    ),
     transports: [
         new winston.transports.Console({
             level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
-            format: winston.format.cli(),
-        })
+        }),
         // new winston.transports.File({ 
         //     filename: 'application.log',
         //     format: winston.format.combine(
